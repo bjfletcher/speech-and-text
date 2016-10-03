e72786bd969a4b3066f5fd3894e3d0aa87ed94ff
Ben Fletcher <ben.fletcher@ft.com>
2016-10-03 10:11:56 +0100

    web stuff for testing

---
+++ script.js
@@ -0,0 +1,61 @@
+var transcript = {
+    final: '',
+    beta: '',
+    alpha: ''
+}
+
+var elements = {
+    final: document.querySelector('[data-transcript-final]'),
+    beta: document.querySelector('[data-transcript-beta]'),
+    alpha: document.querySelector('[data-transcript-alpha]')
+};
+
+var textNodes = {
+    final: document.createTextNode(''),
+    beta: document.createTextNode(''),
+    alpha: document.createTextNode('')
+};
+
+elements.final.appendChild(textNodes.final);
+elements.beta.appendChild(textNodes.beta);
+elements.alpha.appendChild(textNodes.alpha);
+
+var ws = new WebSocket('ws://10.113.193.100:8080');
+
+ws.onmessage = function(m) {
+
+	var isScrolledToBottom = (window.innerHeight + window.scrollY) >= document.body.scrollHeight;
+
+    var results = JSON.parse(m.data).results;
+    if (results.length) {
+        transcript.alpha = '';
+        transcript.beta = '';
+        if (results[0].isFinal) {
+//            transcript.final += ' (' + results[0].confidence.toFixed(1) + ')… ' + results[0].transcript;
+            transcript.final += results[0].transcript + '… ';
+            textNodes.beta.nodeValue = '';
+            textNodes.alpha.nodeValue = '';
+//            textNodes.final.nodeValue += ' (' + results[0].confidence + ') … ' + results[0].transcript;
+            textNodes.final.nodeValue += results[0].transcript + '… ';
+        } else {
+            for (var i = 0; i < results.length; i++) {
+                var result = results[i];
+                if (result.stability > 0.5) {
+                    transcript.beta = result.transcript;
+                } else {
+                    transcript.alpha = result.transcript;
+                }
+            }
+            if (textNodes.beta.nodeValue.length !== transcript.beta.length) {
+                textNodes.beta.nodeValue += transcript.beta.substr(textNodes.beta.nodeValue.length);
+            }
+            if (textNodes.alpha.nodeValue !== transcript.alpha) {
+                textNodes.alpha.nodeValue = transcript.alpha;
+            }
+        }
+        if (isScrolledToBottom) {
+            window.scrollTo(0, document.body.scrollHeight);
+        }
+    }
+
+};
