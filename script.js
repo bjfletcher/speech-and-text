de06a992bbe3f7a388c79f6e17b1b0a9cfb8cf03
Ben Fletcher <ben.fletcher@ft.com>
2016-10-05 16:44:49 +0100

    new address

--- script.js
+++ script.js
@@ -20,7 +20,11 @@ elements.final.appendChild(textNodes.final);
 elements.beta.appendChild(textNodes.beta);
 elements.alpha.appendChild(textNodes.alpha);
 
-var ws = new WebSocket('wss://10.113.193.100:8080');
+var ws = new WebSocket('wss://10.113.193.100:8443')
+
+ws.onerror = function(e) {
+	console.log(e);
+}
 
 ws.onmessage = function(m) {
 
